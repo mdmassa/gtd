45d213137d0d883bfacb33dceae5b5bf36861a25
mdmassa <djsheepwolfbr@gmail.com>
2026-05-24 23:14:35 -0300

    feat(tasks): criando a migração da task

---
+++ gtd-backend/database/migrations/2026_05_23_195544_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('content');
+            $table->string('status')->default('captured');
+            $table->integer('estimated_minutes')->nullable();
+            $table->string('scheduled_day')->nullable();
+            $table->integer('scheduled_order')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
